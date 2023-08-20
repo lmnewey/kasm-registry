07e49d8a0e45bb04a3dad6af8f178d2289a1e9e8
lmnewey <lmnewey@hotmail.com>
2023-08-20 15:39:00 +0800

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,10 +2,10 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Testing a Kasm Technologies Registry',
+    description: 'a test of Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://lmnewey.github.io/kasm-registry/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
